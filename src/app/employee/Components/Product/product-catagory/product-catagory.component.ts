9a176dce9850f3e5d5af7c516be9af1d0aa4945a
Sanjida Sarmin <sanjidasarmin11@gmail.com>
2025-01-26 16:40:20 +0600

    catagory add

--- src/app/employee/Components/Product/product-catagory/product-catagory.component.ts
+++ src/app/employee/Components/Product/product-catagory/product-catagory.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CatagoryService } from 'src/app/employee/Service/Catagory/catagory.service';
+import { Category } from 'src/app/Models/Catagory';
 
 @Component({
   selector: 'app-product-catagory',
   templateUrl: './product-catagory.component.html',
   styleUrls: ['./product-catagory.component.scss']
 })
-export class ProductCatagoryComponent {
-  categories = [
-    { name: 'demo', code: '-', details: '-' },
-    { name: 'bangla book', code: '-', details: '-' },
-    { name: 'Thai Jewellery', code: '-', details: '-' },
-    { name: 'Over Size Bangles', code: '-', details: '-' },
-    { name: 'Baby Bangles', code: '-', details: '-' },
-    { name: 'Jewellery', code: '-', details: '-' }
-  ];
+export class ProductCatagoryComponent  implements OnInit{
+    constructor(
+      private catagoryservice : CatagoryService
+     ){}
+     
+     catagoryList: Category[] = [];
+  
+    ngOnInit(): void {
+       this.catagoryservice.getAllData().subscribe((val : any) => {
+        this.catagoryList = val  
+      })
+    }
+  
+    deletereturn(id : any){
+      this.catagoryservice.deleteById(id).subscribe((val : any) =>{
+        console.log("Data deleted");
+        this.ngOnInit()
+      })
+     }
   
   searchTerm = '';
-  filteredCategories = [...this.categories];
+  filteredCategories = [...this.catagoryList];
 
   search() {
-    this.filteredCategories = this.categories.filter(category =>
+    this.filteredCategories = this.catagoryList.filter(category =>
       category.name.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
   }
--- src/app/employee/Service/Catagory/catagory.service.ts
+++ src/app/employee/Service/Catagory/catagory.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,28 @@ import { Injectable } from '@angular/core';
 })
 export class CatagoryService {
 
-  constructor() { }
-}
+ constructor(private httpClient: HttpClient) { }
+ 
+   private apiUrl = "http://localhost:8081/api/category";
+ 
+   addData(catagoryData: any){
+     return this.httpClient.post(this.apiUrl, catagoryData)
+   }
+ 
+   getAllData(){
+     return this.httpClient.get(this.apiUrl)
+   }
+ 
+   getById(id: any){
+     return this.httpClient.get(this.apiUrl +"/"+ id) 
+   }
+ 
+   deleteById(id:any){
+     return this.httpClient.delete(this.apiUrl+"/"+ id)
+   }
+ 
+   updateData(catagoryData: any){
+     return this.httpClient.put(this.apiUrl+"/"+catagoryData.id, catagoryData)
+   }
+  }
+
